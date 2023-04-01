8f8a81f6aeb54387352d85c7d3d393d5ccce69c6
DayDream-21 <vova13158@gmail.com>
2023-04-01 15:17:42 +0300

    statistics

--- src/main/java/com/slavamashkov/cgmtest/controller/StatisticController.java
+++ src/main/java/com/slavamashkov/cgmtest/controller/StatisticController.java
@@ -14,7 +14,7 @@ import java.util.Map;
 import java.util.stream.Collectors;
 
 @RestController
-@RequestMapping("/api/statistic")
+@RequestMapping("/api/statistics")
 public class StatisticController {
     private static Map<Character, List<Stat>> responseMap = new HashMap<>();
 
